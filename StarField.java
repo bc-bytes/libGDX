b4592bd761d0a0652fa6c8333b077be14cbb670b
bc-bytes <30415247+bc-bytes@users.noreply.github.com>
2017-07-24 20:51:37 +0100

    Updated Javadocs

--- StarField.java
+++ StarField.java
@@ -8,6 +8,11 @@ import com.badlogic.gdx.math.Vector2;
 import java.util.ArrayList;
 import java.util.List;
 
+/** 
+* MyStar class represents a single star object in the starfield.
+*
+* @author bc_bytes
+*/
 class MyStar
 {
     public Color color;
@@ -16,27 +21,35 @@ class MyStar
     public float xOffset;
     public float yOffset;
 
+    /** 
+    * MyStar class constructor.
+    * 
+    * @param position Vector2 containing the starting position of the star object.
+    * @param velocity Vector2 containing the horizontal and vertical velocity of the star object.
+    * @param opacity  A float representing the opacity of the star object.
+    */
     public MyStar(Vector2 position, Vector2 velocity, float opacity)
     {
-        this.position = new Vector2();
-        this.velocity = new Vector2();
-
-        this.position.x = position.x;
-        this.position.y = position.y;
-        this.velocity.x = velocity.x;
-        this.velocity.y = velocity.y;
-
+        this.position = new Vector2(position.x, position.y);
+        this.velocity = new Vector2(velocity.x, velocity.y);
         this.color = new Color(1, 1, 1, opacity);
     }
 }
 
+/** 
+* StarField class processed and draws a custom starfield containing scrolling stars. Starfield simulates three parallax layers.
+* 
+* Uses snippets from https://stackoverflow.com/questions/31889513/canvas-rotating-star-field
+*
+* @author bc_bytes
+*/
 public class StarField
 {
     private final List<MyStar> stars;
     private final Vector2 vect2;
     private final Sprite starSpriteA;
     private final Sprite starSpriteB;
-    private final Rectangle area; // rectangle area that the star field will be plotted in
+    private final Rectangle area;
     private final StarType starType;
     enum StarType
     {
@@ -44,11 +57,19 @@ public class StarField
         SMALL
     }
 
-    public StarField(Assets assets, int starCount, Rectangle area, StarType starType)
+    /** 
+    * StarField class constructor.
+    * 
+    * @param spriteAtlas A SpriteAtlas object containing the sprites of the star objects.
+    * @param starCount   The total number of star objects that the starfield will contain.
+    * @param area        A Rectangle object that will restrict where on the screen the starfield will be plotted.
+    * @param starType    What type of star will be used in the starfield.
+    */
+    public StarField(SpritesAtlas spritesAtlas, int starCount, Rectangle area, StarType starType)
     {
         this.area = area;
-        starSpriteA = assets.spritesAtlas.createSprite("Star1");
-        starSpriteB = assets.spritesAtlas.createSprite("WhitePixel");
+        starSpriteA = spritesAtlas.createSprite("Star1");
+        starSpriteB = spritesAtlas.createSprite("Star2");
         this.starType = starType;
 
         stars = new ArrayList();
@@ -76,11 +97,18 @@ public class StarField
         }
     }
 
+    /** 
+    * Update method updates the positions of all stars in the starfield. Call this every frame in your main update method.
+    * Use the offset parameters if the starfield area position changes.
+    * 
+    * @param xOffset            Offset position for x-axis of the star objects.
+    * @param yOffset            Offset position for y-axis of the star objects.
+    * @param velocityMultiplier Set this value to change the velocity of all star objects.
+    */
     public void Update(float xOffset, float yOffset, float velocityMultiplier)
     {
         for (MyStar star : stars)
         {
-            // star.position.y += (star.velocity.y * Gdx.graphics.getDeltaTime());
             star.xOffset = xOffset;
             star.yOffset = yOffset;
             star.position.y += (star.velocity.y * velocityMultiplier);
@@ -93,20 +121,24 @@ public class StarField
         }
     }
 
+    /** 
+    * Draw method draws all star objects in the starfield to screen. Call this every frame in your main draw method.
+    * 
+    * @param spriteBatch An initialised SpriteBatch object.
+    */
     public void Draw(SpriteBatch spriteBatch)
     {
         for (MyStar star : stars)
         {
+            starSpriteA.setColor(star.color.r, star.color.g, star.color.b, star.color.a);
+            starSpriteA.setPosition(star.position.x + star.xOffset, star.position.y + star.yOffset);
+            
             if (starType == StarType.BIG)
-            {
-                starSpriteA.setColor(star.color.r, star.color.g, star.color.b, star.color.a);
-                starSpriteA.setPosition(star.position.x + star.xOffset, star.position.y + star.yOffset);
+            {                
                 starSpriteA.draw(spriteBatch);
             }
             else
             {
-                starSpriteB.setColor(star.color.r, star.color.g, star.color.b, star.color.a);
-                starSpriteB.setPosition(star.position.x + star.xOffset, star.position.y + star.yOffset);
                 starSpriteB.draw(spriteBatch);
             }
         }
